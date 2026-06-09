d919f5fc10ac0839fc8599019c436e5c7622ad39
xXCaptainJhonaXx <fabrizzioachabal070601@gmail.com>
2026-06-09 00:51:02 -0400

    cors fix

--- src/main/java/io/zhc1/realworld/controller/api/v1/AuthController.java
+++ src/main/java/io/zhc1/realworld/controller/api/v1/AuthController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
